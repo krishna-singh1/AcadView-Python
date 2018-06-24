12142ca8ad2ef27b5b59edf61037e13afe7ce202
Krishna Singh <krishnasingh.ldh@gmail.com>
2018-06-24 14:13:10 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  7 11:46:13 2018
+
+@author: KrRish
+"""
+
+#1. print anything you want on screen
+print("krrish") 
+
+#2. join two string using '+'
+print("first" + "name")
+
+#3. take the input of 3 variables x,y and z. print their valyes on screen
+x=int(input("x = "))
+y=int(input("y = "))
+z=int(input("z = "))
+x
+y
+z
+
+#4. print "let's get started" on screen
+print("\"let's get started\"")
+
+#5. print the following values using placeholder
+name="Acadview"
+c="Python"
+f=5000
+print("name\t=  %s\ncourse\t=  %s\nfees\t=  %s \n" %(name,c,f))
+
+#6. let's do some interesting exercise:
+name="Tony Stark"
+salary=1000000
+print(('%s''%d')%(name,salary))
